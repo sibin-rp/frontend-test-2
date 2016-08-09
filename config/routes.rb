4e84600ae8656007d1debd2963201f2dc97c878d
sibin-rp <sibin@redpanthers.co>
2016-08-09 19:27:15 +0530

    Create home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  get 'home/index'
+
+  get 'home/about'
+
+  get 'home/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
