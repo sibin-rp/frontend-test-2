8397060071520bb9e69a2b6ef0f5d7dd9c035f75
Sibin Xavier <sibin@redpanthers.co>
2016-08-17 16:30:46 +0530

    Bootstrap and Foundation integrated

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,8 @@
+class DashboardController < ApplicationController
+  layout "dashboard" 
+  def index
+  end
+
+  def documentation
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'dashboard/index'
+
+  get 'dashboard/documentation'
+
   devise_for :users
   resources :teams
   resources :posts
